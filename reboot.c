726f5744659ce9e30ac61f932942b26f33265084
nemanjan00 <nemanjan00@gmail.com>
2017-08-22 02:52:47 +0200

    Error debugging

--- reboot.c
+++ reboot.c
@@ -1,14 +1,20 @@
 #include <unistd.h>
+#include <stdio.h>
 #include <linux/reboot.h>
-
-void sync(void);
-int reboot(int magic, int magic2, int flag, void *arg);
+#include <errno.h>
 
 int main(){
 	char bootloader[] = {'b', 'o', 'o', 't', 'l', 'o', 'a', 'd', 'e', 'r', '\0'};
+
+	int res;
 	
 	sync();
-	reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_RESTART2, bootloader);
+
+	res = reboot(LINUX_REBOOT_MAGIC1, LINUX_REBOOT_MAGIC2, LINUX_REBOOT_CMD_RESTART2, &bootloader);
+
+	if(res == -1){
+		perror("reboot()");
+	}
 
 	return 0;
 }
